dc1d5463c7cbde71ecc533bc505ff6916f5e545b
asfilatov <a.filatov@postgrespro.ru>
2026-07-31 10:16:43 +0300

    add extra packages for pip cache

--- sources/tengri-python/files/install.sh
+++ sources/tengri-python/files/install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# cspell:ignore ipykernel tngri
+# cspell:ignore ipykernel tngri altair matplotlib pyiceberg
 set -eu
 mkdir -p /home/python/packages
 pip wheel --no-deps -w /home/python/packages \
   git+https://github.com/TengriDataPlatform/tngri_client.git
-pip download -d /home/python/packages --find-links /home/python/packages ipykernel tngri
+pip download -d /home/python/packages --find-links /home/python/packages \
+  ipykernel tngri altair matplotlib prophet pyiceberg requests
 mkdir -p /home/python
 chmod 1777 /tmp
 chmod 1777 /home/python
